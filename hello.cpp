73b4313ed5cc1d179d8f11dfcbcce2933e5b20b8
iamAzhar <mohdazhar6117@gmail.com>
2021-08-30 21:09:09 +0800

    intro

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+   cout << "Hello world"<< endl;
+   for(int i=0; i<10; i++){
+        cout << "i could die in your arms\n";
+   }
+   return 0; 
+}
